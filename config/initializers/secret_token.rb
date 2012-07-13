09a33738bdca20e9393feb1801a0341744c282b1
Gleb Grishin <griglmail@gmail.com>
2012-07-13 11:22:00 +0400

    new synergy project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knigipodarki::Application.config.secret_token = '9252035ffe1214ad64b6517d61119966621f75dc8e1256773653fb31098bbed577e6f37679b9b1899dae290d4fb8ec3a1edb317fa9e6adc20d001b53e006bf5f'
