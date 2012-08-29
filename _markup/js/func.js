50adc63c3648e878de52f900e04ad951accf603e
Mihail Yakimenko <macgera@gmail.com>
2012-08-29 14:22:16 +0300

    clean code

--- _markup/js/func.js
+++ _markup/js/func.js
@@ -5,9 +5,6 @@ var SortLinks = function(){
         }[this.className];
         return false;
     });
-}
-
-var ResetSorting = function() {
     $('reset-sorting').click(function() {
         $('.sorting li a').removeClass('none').removeClass('up').removeClass('down').addClass('none');
         return false;
