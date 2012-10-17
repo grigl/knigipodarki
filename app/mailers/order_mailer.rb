d9bd9ce0fb02bdef47180835a6137c414d928f9b
gleb grishin <griglmail@gmail.com>
2012-10-17 19:55:41 +0400

    письмо подтверждения заказа

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,21 @@
+class OrderMailer < ActionMailer::Base
+  helper "spree/base"
+  default_url_options[:host] = Spree::Config[:site_url]
+
+  def confirm_email(order, resend=false)
+    @order = order
+    subject = (resend ? "[RESEND] " : "")
+    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope =>'order_mailer.confirm_email')} ##{order.number}"
+    mail(:to => order.email,
+         :subject => subject,
+         :template_name => 'confirm')
+  end
+
+  def cancel_email(order, resend=false)
+    @order = order
+    subject = (resend ? "[RESEND] " : "")
+    subject += "#{Spree::Config[:site_name]} #{t('subject', :scope => 'order_mailer.cancel_email')} ##{order.number}"
+    mail(:to => order.email,
+         :subject => subject)
+  end
+end
