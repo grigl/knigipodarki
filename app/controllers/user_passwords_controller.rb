4230675579f50c6eb6adc96484d29213e7ffb551
gleb grishin <griglmail@gmail.com>
2012-11-02 02:38:34 +0400

    нельзя восстановит пароль введя пустой. сообщение об ошибке токена

--- app/controllers/user_passwords_controller.rb
+++ app/controllers/user_passwords_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class UserPasswordsController < Devise::PasswordsController
   include SpreeBase
   helper :users, 'spree/base'
@@ -29,6 +30,22 @@ class UserPasswordsController < Devise::PasswordsController
   end
 
   def update
-    super
+    unless params[:user][:password].empty? && params[:user][:password_confirmation].empty?
+      self.resource = resource_class.reset_password_by_token(params[resource_name])
+
+      if resource.errors.empty?
+        set_flash_message(:notice, :updated) if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => redirect_location(resource_name, resource)
+      else
+        respond_with_navigational(resource){ render_with_scope :edit }
+      end
+    else
+      @user = User.find_by_reset_password_token(params[:user][:reset_password_token])
+      @user ||= User.new
+      
+      @error = 'Вы не ввели пароль'
+      respond_with_navigational(resource){ render_with_scope :edit }
+    end
   end
 end
