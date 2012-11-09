b55205ef7eb211757c2d96c44c20b293f468d998
Mihail Yakimenko <macgera@gmail.com>
2012-11-09 12:35:04 +0200

    #19643378 fix it

--- public/javascripts/js/func.js
+++ public/javascripts/js/func.js
@@ -138,11 +138,13 @@ var MaskInp = function() {
 }
 
 var RefreshCart = function() {
-  var NoActive = $('.refresh').removeClass('active');
+  var NoActive = function(){ $('.refresh').removeClass('active'); }
+
+  // При клике добавляем класс .active и через 0.3 c убираем 
   $('.add-to-cart').click(function() {
     $('.refresh').addClass('active');
+    setTimeout(NoActive, 300);
   });
-  setTimeout(NoActive, 300);
 }
 
 $(function(){
