0f3d75f9d6595ced06cc26a79d76b1e29b7e6ce8
Gray Hound <admin@devilmaydie.name>
2013-01-10 14:41:51 +0400

    middlename not required

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 Address.class_eval do
-  validates :secondname, presence: true
+  #validates :secondname, presence: true
   validates :zipcode, format: { with: /[0-9]+/, message: 'может содержать только цифры' }
   validates :phone, format: { with: /^[-+()\/\s\d]+$/, message: 'имеет неверный формат' }
 
