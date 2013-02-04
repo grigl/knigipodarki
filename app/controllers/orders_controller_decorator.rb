b9b391f0a001274d85b15a4d34a1b60411031de7
Gray Hound <admin@devilmaydie.name>
2013-02-04 16:42:05 +0400

    payment method id

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -149,7 +149,7 @@ OrdersController.class_eval do
   
   def sberbank_invoice
     @order = Order.find_by_number(params[:id])
-    if not @order.payment_method
+    if not @order.payment_method_id
       render_404
     end
     if not @order.payment_method.is_a?(PaymentMethod::SberBankInvoice)
