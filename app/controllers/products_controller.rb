745c4eb270746d7fb02509e19d470ae1a7c0e9ce
Gray Hound <admin@devilmaydie.name>
2013-02-06 16:07:33 +0400

    sort fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,9 +66,9 @@ class ProductsController < Spree::BaseController
       end
     else
       if params[:order] == 'DESC'
-        products = products.sort_by! { |product| product.created_at }.reverse
+        products = products.sort_by! { |product| [product.created_at, product.id] }.reverse
       else
-        products = products.sort_by! { |product| product.created_at }
+        products = products.sort_by! { |product| [product.created_at, product.id] }
       end
     end
 
