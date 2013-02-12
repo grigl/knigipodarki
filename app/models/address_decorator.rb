f109c59ba764d3ca21e9706ecb3e722b623967f7
Gray Hound <admin@devilmaydie.name>
2013-02-12 13:13:11 +0400

    allow blank

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -2,10 +2,10 @@
 Address.class_eval do
   #validates :secondname, presence: true
   validates :firstname, :lastname, :city, :country, :phone, :presence => true
-  validates :zipcode, :address1, :presence => false
-  validates :zipcode, format: { with: /[0-9]+/, message: 'может содержать только цифры' }
+  validates :zipcode, format: { with: /[0-9]+/, message: 'может содержать только цифры' }, :allow_blank => true
   validates :phone, format: { with: /^[-+()\/\s\d]+$/, message: 'имеет неверный формат' }
-
+  validates :address1, :allow_blank => true
+  
   def to_s
     "#{lastname} #{firstname} #{secondname}: #{zipcode}, #{country}, #{state || state_name}, #{city}, #{address1}"
   end
