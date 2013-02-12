f1c29c655fa27a5d3c157e02862b2597986148cb
Gray Hound <admin@devilmaydie.name>
2013-02-12 12:38:22 +0400

    zipcode and address1 not required anymore

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 Address.class_eval do
   #validates :secondname, presence: true
+  validates :firstname, :lastname, :city, :country, :phone, :presence => true
   validates :zipcode, format: { with: /[0-9]+/, message: 'может содержать только цифры' }
   validates :phone, format: { with: /^[-+()\/\s\d]+$/, message: 'имеет неверный формат' }
 
