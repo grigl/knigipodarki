2de2517ca41fb5fd53af782ce0ed478c7008eaff
Gray Hound <admin@devilmaydie.name>
2013-02-12 13:16:05 +0400

    length_max

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -4,7 +4,7 @@ Address.class_eval do
   validates :firstname, :lastname, :city, :country, :phone, :presence => true
   validates :zipcode, format: { with: /[0-9]+/, message: 'может содержать только цифры' }, :allow_blank => true
   validates :phone, format: { with: /^[-+()\/\s\d]+$/, message: 'имеет неверный формат' }
-  validates :address1, :allow_blank => true
+  validates :address1, :length => { :maximum => 999 },  :allow_blank => true
   
   def to_s
     "#{lastname} #{firstname} #{secondname}: #{zipcode}, #{country}, #{state || state_name}, #{city}, #{address1}"
