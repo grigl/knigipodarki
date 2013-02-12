836fcd8067b97b510761caa56f0c02464b08a95c
Gray Hound <admin@devilmaydie.name>
2013-02-12 12:47:49 +0400

    validated

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -2,7 +2,7 @@
 Address.class_eval do
   #validates :secondname, presence: true
   validates :firstname, :lastname, :city, :country, :phone, :presence => true
-  validated :zipcode, :address1, :presence => false
+  validates :zipcode, :address1, :presence => false
   validates :zipcode, format: { with: /[0-9]+/, message: 'может содержать только цифры' }
   validates :phone, format: { with: /^[-+()\/\s\d]+$/, message: 'имеет неверный формат' }
 
