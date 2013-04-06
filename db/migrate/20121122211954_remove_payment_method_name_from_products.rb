fcd30cc567faf4295786d55f42e81237af306f3f
Gleb Grishin <griglmail@gmail.com>
2013-04-06 23:09:47 +0400

    cleanup

--- db/migrate/20121122211954_remove_payment_method_name_from_products.rb
+++
@@ -1,9 +0,0 @@
-class RemovePaymentMethodNameFromProducts < ActiveRecord::Migration
-  def self.up
-    remove_column :products, :payment_method_name
-  end
-
-  def self.down
-    add_column :products, :payment_method_name, :string
-  end
-end
